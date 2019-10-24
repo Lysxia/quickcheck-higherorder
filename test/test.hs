c7ba923435cce955f3d7fce4fbee2398872c53de
Lysxia <lysxia@gmail.com>
2019-10-24 19:03:05 -0400

    Example bad property for higher-order functions

--- test/test.hs
+++ test/test.hs
@@ -54,9 +54,15 @@ testFunctionPretty = testGroup "pretty"
 trivialProperty :: (Eq a, Show a) => ((a -> a) -> a) -> Property
 trivialProperty f = f id === f id
 
+badProperty :: (Eq a, Show a) => ((a -> a) -> a) -> (a -> a) -> Property
+badProperty f g = expectFailure (f id === f g)
+
 testFunctionQC :: TestTree
 testFunctionQC = testGroup "qc"
   [ testProperty "trivial-Int"    (property' (trivialProperty @Int))
   , testProperty "trivial-Bool"   (property' (trivialProperty @Bool))
   , testProperty "trivial-Either" (property' (trivialProperty @(Either () ())))
+  , testProperty "bad-Int"    (property' (badProperty @Int))
+  , testProperty "bad-Bool"   (property' (badProperty @Bool))
+  , testProperty "bad-Either" (property' (badProperty @(Either () ())))
   ]
