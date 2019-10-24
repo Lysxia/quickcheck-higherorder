5c0d5deb3c51757769dd378f2412fb60c2c0d200
Lysxia <lysxia@gmail.com>
2019-10-24 12:13:56 -0400

    Pretty printing of function representations

--- src/Test/QuickCheck/HigherOrder/Function.hs
+++ src/Test/QuickCheck/HigherOrder/Function.hs
@@ -7,6 +7,7 @@
 
 module Test.QuickCheck.HigherOrder.Function where
 
+import Control.Applicative (liftA2)
 import Data.Void
 import Data.Kind (Type)
 
@@ -22,6 +23,9 @@ type ConName = String
 
 newtype Repr_ a = Repr_ (Repr a)
 
+showRepr_ :: Show (Repr a) => Repr_ a -> Int -> String
+showRepr_ (Repr_ r) d = showsPrec d r ""
+
 instance Arbitrary (Repr a) => Arbitrary (Repr_ a) where
   arbitrary = Repr_ <$> arbitrary
   shrink (Repr_ r) = Repr_ <$> shrink r
@@ -30,9 +34,9 @@ data a :-> r where
   Const :: r -> a :-> r
   CoApply :: Constructible a => Repr_ a -> (b :-> (a -> b) :-> r) -> (a -> b) :-> r
   Apply :: FunName -> (a -> b) -> (b :-> r) -> (a :-> r)
-  Case :: TypeName -> (a -> x) -> Branches x r -> r -> (a :-> r)
-  CaseInteger :: TypeName -> (a -> Integer) -> Bin r -> r -> (a :-> r)
-  Absurd :: Void :-> r
+  Case :: TypeName -> (a -> x) -> r -> Branches x r -> (a :-> r)
+  CaseInteger :: TypeName -> (a -> Integer) -> r -> Bin r -> (a :-> r)
+  Absurd :: (a -> Void) -> a :-> r
 
 data Branches x r where
   Fail :: Branches x r
@@ -49,13 +53,13 @@ apply :: FunName -> (a -> b) -> (b :-> r) -> (a :-> r)
 apply _ _ (Const r) = Const r
 apply fn f h = Apply fn f h
 
-case_ :: TypeName -> (a -> x) -> Branches x r -> r -> (a :-> r)
-case_ _ _ Fail r = Const r
-case_ tn f b r = Case tn f b r
+case_ :: TypeName -> (a -> x) -> r -> Branches x r -> (a :-> r)
+case_ _ _ r Fail = Const r
+case_ tn f r b = Case tn f r b
 
-caseInteger :: TypeName -> (a -> Integer) -> Bin r -> r -> (a :-> r)
-caseInteger _ _ BinEmpty r = Const r
-caseInteger tn f b r = CaseInteger tn f b r
+caseInteger :: TypeName -> (a -> Integer) -> r -> Bin r -> (a :-> r)
+caseInteger _ _ r BinEmpty = Const r
+caseInteger tn f r b = CaseInteger tn f r b
 
 alt :: Branches x r -> Branches y r -> Branches (Either x y) r
 alt Fail Fail = Fail
@@ -69,15 +73,141 @@ data Bin r
   = BinEmpty
   | BinAlt r (Bin r) (Bin r)
 
+---
+
+data Var = Var String !Int
+
+type DString = String -> String
+
+type PrecDString = Int -> DString
+
+sid :: DString
+sid = id
+
+sstring :: String -> DString
+sstring = (++)
+
+(%) :: DString -> DString -> DString
+(%) = (.)
+
+(~%) :: String -> DString -> DString
+(~%) = (%) . sstring
+
+infixr 1 %, ~%
+
+sparens :: Int -> DString -> PrecDString
+sparens d0 s d = if d > d0 then "(" ~% s % ")" ~% sid else s
+
+newtype Expr = Expr { unExpr :: PrecDString }
+
+unExpr_ :: Expr -> DString
+unExpr_ e = unExpr e 0
+
+type Pattern = Expr
+
+-- Context mapping variables to expressions.
+data Ctx = (Var, Expr) :. Ctx
+
+type C a = Ctx -> a
+
+tWild :: Pattern
+tWild = Expr (\_ -> sstring "_")
+
+tConst :: String -> Expr
+tConst s = Expr (\_ -> sstring s)
+
+eConst :: String -> C Expr
+eConst s _ = tConst s
+
+tApp :: Expr -> Expr -> Expr
+tApp f x = Expr (sparens 10 (unExpr f 10 % " " ~% unExpr x 11))
+
+-- The patterns are parameterized by a fresh variable.
+type CBranches = Var -> C [(Pattern, Expr)]
+
+eCase :: TypeName -> C Expr -> CBranches -> C Expr
+eCase tn r bs ((v, t) :. vs) = Expr (\_ ->
+    "case " ~% unExpr_ t % " :: " ~% tn ~% " of { "
+      ~% sBranches
+      % " }" ~% sid)
+  where
+    p ?-> e = unExpr_ p % " -> " ~% unExpr_ e
+    sBranches =
+      foldr (\(p, e) ebs -> (p ?-> e) % " ; " ~% ebs)
+        (tWild ?-> r vs)
+        (bs v vs)
+
+eApply :: FunName -> C Expr -> C Expr
+eApply f y ((v, t) :. vs) =
+  y ((v, tApp (tConst f) t) :. vs)
+
+eCoApply :: Show (Repr a) => Repr_ a -> C Expr -> C Expr
+eCoApply a y ((v, t) :. vs) =
+  y ((v, tApp t (tConst (showRepr_ a 11))) :. vs)
+
+eAbsurd :: C Expr
+eAbsurd ((_, t) :. _) = Expr (\_ -> "case " ~% unExpr_ t % " of {}" ~% sid)
+
+eFun :: forall a r. (r -> C Expr) -> (a :-> r) -> C Expr
+eFun prettyR = go where
+  go :: forall b. (b :-> r) -> C Expr
+  go (Absurd _) = eAbsurd
+  go (Const r) = prettyR r
+  go (CoApply a h) = eCoApply a (eFun go h)
+  go (Apply fn _ h) = eApply fn (go h)
+  go (Case tn _ r b) = eCase tn (prettyR r) (eBranches prettyR b)
+  go (CaseInteger tn _ r b) = eCase tn (prettyR r) undefined
+
+prettyFun :: forall a r. (r -> C Expr) -> (a :-> r) -> String
+prettyFun prettyR h = unExpr_ (eFun prettyR h defCtx) ""
+
+defCtx :: Ctx
+defCtx = addVar [Var "a" 0] badCtx
+
+badCtx :: Ctx
+badCtx = addVar [Var "unknown" 0] badCtx
+
+eBranches :: forall x r. (r -> C Expr) -> Branches x r -> CBranches
+eBranches prettyR = go where
+  go :: forall y. Branches y r -> CBranches
+  go Fail = \_ _ -> []
+  go (Alt b1 b2) = (liftA2 . liftA2) (++) (go b1) (go b2)
+  go (Pat cn d) = eFields prettyR d cn []
+
+eFields :: forall x r. (r -> C Expr) -> Fields x r -> ConName -> [Var] -> CBranches
+eFields prettyR = go where
+  go :: forall y. Fields y r -> ConName -> [Var] -> CBranches
+  go (NoField h) cn ps _ vs = [(mkPattern cn ps, prettyR h (addVar ps vs))]
+  go (Field d) cn ps v vs = eFields (eFun prettyR) d cn (v' : ps) v' vs where
+    v' = nextVar v
+
+nextVar :: Var -> Var
+nextVar (Var s i) = Var s (i + 1)
+
+mkPattern :: ConName -> [Var] -> Pattern
+mkPattern cn vs = Expr (\_ -> cn ~% foldr (\v s -> " " ~% sVar v % s) sid vs)
+
+sVar :: Var -> DString
+sVar (Var s i) = s ~% show i ~% sid
+
+eVar :: Var -> Expr
+eVar v = Expr (\_ -> sVar v)
+
+addVar :: [Var] -> Ctx -> Ctx
+addVar [] vs = vs
+addVar (v : ps) vs = (v, eVar v) :. addVar ps vs
+
+---
+
 shrinkFun :: forall a r. (r -> [r]) -> (a :-> r) -> [a :-> r]
 shrinkFun shrinkR = go where
   go :: forall b. (b :-> r) -> [b :-> r]
-  go Absurd = []
+  go (Absurd _) = []
   go (Const r) = fmap Const (shrinkR r)
   go (CoApply a h) = fmap (coapply a) (shrinkFun go h) ++ fmap (\a' -> CoApply a' h) (shrink a)
   go (Apply fn f h) = apply fn f <$> go h
-  go (Case tn f b r) = Const r : fmap (\b' -> case_ tn f b' r) (shrinkBranches shrinkR b)
-  go (CaseInteger tn f b r) = root b : fmap (\b' -> caseInteger tn f b' r) (shrinkBin shrinkR b) where
+  go (Case tn f r b) = Const r : fmap (\b' -> case_ tn f r b') (shrinkBranches shrinkR b)
+  go (CaseInteger tn f r b) = root b : fmap (\b' -> caseInteger tn f r b') (shrinkBin shrinkR b) where
     root BinEmpty = Const r
     root (BinAlt r' _ _) = Const r'
 
---
+++ test/test.hs
@@ -0,0 +1,35 @@
+{-# LANGUAGE TypeOperators #-}
+
+module Main where
+
+import Test.Tasty
+import Test.Tasty.HUnit
+
+import Test.QuickCheck.HigherOrder.Function
+
+main :: IO ()
+main = defaultMain tests
+
+tests :: TestTree
+tests = testGroup "tests"
+  [ testFunction
+  ]
+
+testFunction :: TestTree
+testFunction = testGroup "Function"
+  [ testFunctionPretty
+  ]
+
+prettyFun_ :: (a :-> String) -> String
+prettyFun_ = prettyFun eConst
+
+testFunctionPretty :: TestTree
+testFunctionPretty = testGroup "pretty"
+  [ testCase "example"
+      $ "case a0 :: Either _ _ of { Left a1 -> 0 ; Right a1 -> case a1 of {} ; _ -> 0 }"
+      @=? prettyFun_
+        (Case "Either _ _" id "0"
+          (Alt
+            (Pat "Left" (Field (NoField (Const "0"))))
+            (Pat "Right" (Field (NoField (Absurd id))))))
+  ]
