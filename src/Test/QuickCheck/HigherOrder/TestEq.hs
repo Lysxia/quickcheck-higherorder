008b22bc75d9d1ba27898eff53d0af1aab6e2447
Lysxia <lysxia@gmail.com>
2019-11-17 16:27:26 -0500

    Add rudimentary tracing in TestEq instances

--- src/Test/QuickCheck/HigherOrder/TestEq.hs
+++ src/Test/QuickCheck/HigherOrder/TestEq.hs
@@ -34,22 +34,29 @@ instance (Constructible a, TestEq b) => TestEq (a -> b) where
 deriving instance TestEq a => TestEq (Identity a)
 
 instance (TestEq a, TestEq b) => TestEq (a, b) where
-  (a1, b1) =? (a2, b2) = a1 =? a2 .&&. b1 =? b2
+  (a1, b1) =? (a2, b2) =
+    counterexample "(_, _) =? (_, _)   (1) ..." (a1 =? a2) .&&.
+    counterexample "(_, _) =? (_, _)   (2) ..." (b1 =? b2)
 
 instance (TestEq a, TestEq b) => TestEq (Either a b) where
-  Left a1 =? Left a2 = a1 =? a2
-  Right b1 =? Right b2 = b1 =? b2
-  _ =? _ = property False
+  Left a1  =? Left a2  = counterexample "Left _ =? Left _   ..."   $ a1 =? a2
+  Right b1 =? Right b2 = counterexample "Right _ =? Right _   ..." $ b1 =? b2
+  Left _ =? Right _ = counterexample "Left _ /= Right _" $ property False
+  Right _ =? Left _ = counterexample "Right _ /= Left _" $ property False
 
 instance TestEq a => TestEq (Maybe a) where
-  Just a =? Just b = a =? b
+  Just a =? Just b = counterexample "Just _ =? Just _   ..." $ a =? b
   Nothing =? Nothing = property True
-  _ =? _ = property False
+  Just _ =? Nothing = counterexample "Just _ /= Nothing" $ property False
+  Nothing =? Just _ = counterexample "Nothing /= Just _" $ property False
 
 instance TestEq a => TestEq [a] where
   [] =? [] = property True
-  a : as =? b : bs = a =? b .&&. as =? bs
-  _ =? _ = property False
+  a : as =? b : bs =
+    counterexample "(_ : _) =? (_ : _)   (1) ..." (a =? b) .&&.
+    counterexample "(_ : _) =? (_ : _)   (2) ..." (as =? bs)
+  [] =? _ : _ = counterexample "[] /= (_ : _)" $ property False
+  _ : _ =? [] = counterexample "(_ : _) /= []" $ property False
 
 instance TestEq Integer where (=?) = decEq
 instance TestEq Int where (=?) = decEq
